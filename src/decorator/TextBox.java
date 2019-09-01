ce1c9fa421693a17ab299f9e825a25641843e6b9
suqian123 <1016102690@qq.com>
2019-09-01 17:09:41 +0800

    内容：textbox扩展到window 提交人：苏倩

--- src/decorator/TextBox.java
+++ src/decorator/TextBox.java
@@ -2,6 +2,15 @@
 package decorator;
 
 public class TextBox extends Component {
+	Window box;
+	TextBox(Window box){
+		this.box=box;
+	}
+	public void clear(){
+		System.out.println( "扩展代码1" );
+		box.clear();
+		System.out.println( "扩展代码2" );
+	}
 	public void display() {
 		System.out.println("显示文本框！");
 	}
