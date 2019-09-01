b33faf3d43e8ee4ed572b362ce4f7e55fe8a9cda
suqian123 <1016102690@qq.com>
2019-09-01 18:30:02 +0800

    内容：Listbox扩展到window 提交人：苏倩

--- src/decorator/Window.java
+++ src/decorator/Window.java
@@ -7,7 +7,9 @@ public class Window extends Component {
 			System.out.println( "TextBox cleared" );
 		}
 
-
+public void click(){
+		    System.out.println( "已选中" );
+}
 	public void display() {
 		System.out.println("显示窗体！");
 	}
