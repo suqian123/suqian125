2b7daa237f75916c40fdb35cfe8a60b764e2faf8
suqian123 <1016102690@qq.com>
2019-09-05 13:00:13 +0800

    内容：第三次修改 提交人：苏倩

--- src/decorator/Client.java
+++ src/decorator/Client.java
@@ -2,11 +2,20 @@
 package decorator;
 
 public class Client {
+	private static Object BlackBorderDecorator;
+
+
 	public static void main(String args[]) {
 		Component component, componentSB, componentBB;  //使用抽象构件定义全部对象
 		component = new Window();         //创建具体构件对象
 		componentSB = new ScrollBarDecorator(component); //创建装饰后的构件对象        
 		componentBB = new BlackBorderDecorator(componentSB); //将装饰了一次的对象注入另一个装饰类中，进行第二次装饰
 		componentBB.display();
+		Window window=new Window();
+Window ScrollbarDecorator=new TextBox( new ListBox(window));
+ScrollbarDecorator.move();
+
 	}
-}
+
+	}
+
